9310c95ea976c756418095e1c84a33bc8435af4f
LeoZion <leozionsuporte@gmail.com>
2021-08-20 02:27:10 -0300

    install ejs config routes and template view

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const server = express()
+const routes = require("./routes")
+
+server.set('view engine', 'ejs')
 
 server.use(express.static("public"))
 
-server.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/views/index.html')
-})
+server.use(routes)
 
 server.listen(3000, () => console.log('Server start ...'))
